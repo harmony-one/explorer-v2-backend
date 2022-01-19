658af9419fe7f4754829561aa06fa01e59322cfa
artemkolodko <temakolodko@gmail.com>
2022-01-19 20:20:41 +0300

    Add second 1wallet address

--- src/store/postgres/OneWalletMetrics.ts
+++ src/store/postgres/OneWalletMetrics.ts
@@ -1,7 +1,10 @@
 import {Query} from 'src/store/postgres/types'
 import {Address} from 'src/types'
 
-const OneWalletAddress = '0xc8cd0c9ca68b853f73917c36e9276770a8d8e4e0'
+const oneWalletAddresses = [
+  '0xc8cd0c9ca68b853f73917c36e9276770a8d8e4e0',
+  '0xe0f4dda31750e410a7cc62f7aa5ae95fa56f050d',
+].map((a) => a.toLowerCase())
 
 export class PostgresStorageOneWalletMetrics {
   query: Query
@@ -12,8 +15,11 @@ export class PostgresStorageOneWalletMetrics {
 
   getWallets = async (): Promise<Address[]> => {
     const res = await this.query(
-      `select "to" from internal_transactions where "from"=$1 and type='create'`,
-      [OneWalletAddress]
+      `
+            select "to" from internal_transactions
+            where "from" = any ($1) and type='create'
+      `,
+      [oneWalletAddresses]
     )
 
     // @ts-ignore
