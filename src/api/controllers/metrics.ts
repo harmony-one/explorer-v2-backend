0f820a180e73ed1b4bbdda9c45252d0c35f76eb0
artemkolodko <temakolodko@gmail.com>
2022-04-01 13:43:33 +0100

    Use custom Metrics dao queries to get txs and wallets data

--- src/api/controllers/metrics.ts
+++ src/api/controllers/metrics.ts
@@ -2,15 +2,29 @@ import {withCache} from 'src/api/controllers/cache'
 import nodeFetch from 'node-fetch'
 
 const MetricsApiUrl = 'https://api.flipsidecrypto.com'
+const TxsCountUrl = `${MetricsApiUrl}/api/v2/queries/fd762b43-f90d-4585-b46c-3c9e81dfb1b0/data/latest`
+const WalletsCountUrl = `${MetricsApiUrl}/api/v2/queries/dc2ff1de-ae3b-4f37-aa0a-ddc690aede4e/data/latest`
+
+/*
+ * How to get metrics url
+ * 1) Go to query editor:
+ *  - https://app.flipsidecrypto.com/velocity/queries/fd762b43-f90d-4585-b46c-3c9e81dfb1b0 (txs count)
+ *  - https://app.flipsidecrypto.com/velocity/queries/dc2ff1de-ae3b-4f37-aa0a-ddc690aede4e (wallets count)
+ *  - or create your own collection
+ * 2) Change query, run it, check result
+ * 3) Click on "API" button to get actual query URL
+ *
+ * Queries created by Metrics DAO: https://harmony-transactions.vercel.app/
+ * */
 
 const timeout = () =>
-  new Promise((resolve, reject) => setTimeout(() => reject(new Error('Connection timeout')), 20000))
+  new Promise((resolve, reject) =>
+    setTimeout(() => reject(new Error('Connection timeout')), 120000)
+  )
 const fetch = (url: string) => nodeFetch(url).then((r) => r.json())
 
-const getMetricsDAOTxsCount = async (daysCount = 14) => {
-  const items: Array<{COUNT: number; DATE: string}> = await fetch(
-    `${MetricsApiUrl}/api/v2/queries/8b1f7226-995c-4091-acc6-84a7f70b9833/data/latest`
-  )
+const getMetricsDAOTxsCount = async () => {
+  const items: Array<{COUNT: number; DATE: string}> = await fetch(TxsCountUrl)
   return items
     .map(({DATE, COUNT}) => {
       return {
@@ -18,13 +32,11 @@ const getMetricsDAOTxsCount = async (daysCount = 14) => {
         count: COUNT.toString(),
       }
     })
-    .slice(-(daysCount + 1))
+    .reverse()
 }
 
-const getMetricsDAOWalletsCount = async (daysCount = 14) => {
-  const items: Array<{ACTIVE_WALLETS: number; DATE: string}> = await fetch(
-    `${MetricsApiUrl}/api/v2/queries/532a5af5-7dc5-4446-be20-ff83a5f32d45/data/latest`
-  )
+const getMetricsDAOWalletsCount = async () => {
+  const items: Array<{ACTIVE_WALLETS: number; DATE: string}> = await fetch(WalletsCountUrl)
   return items
     .map(({DATE, ACTIVE_WALLETS}) => {
       return {
@@ -32,14 +44,14 @@ const getMetricsDAOWalletsCount = async (daysCount = 14) => {
         count: ACTIVE_WALLETS.toString(),
       }
     })
-    .slice(-(daysCount + 1))
+    .reverse()
 }
 
 export async function getTransactionCountLast14Days(): Promise<any | null> {
   return await withCache(
     ['getTransactionCountLast14Days', arguments],
     // () => stores[0].metrics.getTransactionCountLast14Days(),
-    () => Promise.race([getMetricsDAOTxsCount(14), timeout()]),
+    () => Promise.race([getMetricsDAOTxsCount(), timeout()]),
     1000 * 60 * 60 * 1
   )
 }
@@ -48,7 +60,7 @@ export async function getWalletsCountLast14Days(): Promise<any> {
   const count = await withCache(
     ['getWalletsCountLast14Days'],
     // () => stores[0].metrics.getWalletsCountLast14Days(),
-    () => Promise.race([getMetricsDAOWalletsCount(14), timeout()]),
+    () => Promise.race([getMetricsDAOWalletsCount(), timeout()]),
     1000 * 60 * 60 * 1
   )
 
