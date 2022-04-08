db5dd0f54940ad34e08e3e9e8b95ac7b793234c9
Artem <temakolodko@gmail.com>
2022-04-08 18:44:04 +0300

    Add has_internal_values to transactions (#39)
    * Add has_internal_values to transactions
    
    * Add transaction_extra_mark enum
    
    * Fix scheme

--- src/store/postgres/queryMapper.ts
+++ src/store/postgres/queryMapper.ts
@@ -38,6 +38,7 @@ export const mapNaming: Record<string, string> = {
   ipfs_hash: 'IPFSHash',
   last_update_block_number: 'lastUpdateBlockNumber',
   token_uri: 'tokenURI',
+  extra_mark: 'extraMark',
 }
 
 export const mapNamingReverse: Record<string, string> = Object.keys(mapNaming).reduce((a, k) => {
