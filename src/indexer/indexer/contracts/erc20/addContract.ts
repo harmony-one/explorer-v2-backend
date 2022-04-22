91cfa72f6a7464a6dc66b913690d86c2d7dc80df
artemkolodko <temakolodko@gmail.com>
2022-04-22 20:32:10 +0100

    Remove x00 characters on contract create; Update typescript version

--- src/indexer/indexer/contracts/erc20/addContract.ts
+++ src/indexer/indexer/contracts/erc20/addContract.ts
@@ -45,8 +45,8 @@ export const addContract = async (store: PostgresStorage, contract: Contract) =>
   const erc20: IERC20 = {
     address: contract.address,
     decimals: +params.decimals,
-    name: params.name.replace('\u0000', ''),
-    symbol: params.symbol.replace('\u0000', ''),
+    name: params.name.replaceAll('\u0000', ''),
+    symbol: params.symbol.replaceAll('\u0000', ''),
     lastUpdateBlockNumber: contract.blockNumber,
   }
   l.info(`Found new contract "${erc20.name}" at ${contract.blockNumber}`)
