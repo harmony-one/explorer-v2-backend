e8fa73a6de414470a2ec87235ef5de2aa12c9c5a
artemkolodko <temakolodko@gmail.com>
2022-04-25 19:18:31 +0100

    Add temp hardcoded 1wallet values

--- src/api/controllers/oneWalletMetrics.ts
+++ src/api/controllers/oneWalletMetrics.ts
@@ -4,15 +4,16 @@ import {withCache} from 'src/api/controllers/cache'
 import nodeFetch from 'node-fetch'
 
 export async function oneWalletGetMetrics(): Promise<any> {
-  const {count, totalAmount} = await withCache(
-    ['oneWalletGetBalances'],
-    () => stores[0].oneWalletMetrics.getMetrics(),
-    1000 * 60 * 60,
-    false
-  )
+  // TODO: remove hardcode when migration will be finished
+  // const {count, totalAmount} = await withCache(
+  //   ['oneWalletGetBalances'],
+  //   () => stores[0].oneWalletMetrics.getMetrics(),
+  //   1000 * 60 * 60,
+  //   false
+  // )
 
   return {
-    count,
-    totalAmount,
+    count: 9461,
+    totalAmount: '382109092287375261703877',
   }
 }
