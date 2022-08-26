43673329c76e2f24208390847b9fa1b316b39bba
artemkolodko <temakolodko@gmail.com>
2022-08-26 20:38:23 +0100

    Add transactions count metrics persistent store

--- src/indexer/indexer/metrics/stats.ts
+++ src/indexer/indexer/metrics/stats.ts
@@ -10,8 +10,18 @@ export const statsIndexer = () => {
 }
 
 const loop = async () => {
-  const dateStart = Date.now()
-  await stores[0].metrics.updateWalletsStats()
-  l.info(`Wallets stats updated in ${Math.round((Date.now() - dateStart) / 1000)}s`)
-  setTimeout(loop, interval)
+  try {
+    const dateStart = Date.now()
+    await Promise.all([
+      stores[0].metrics.updateWalletsCount(14),
+      stores[0].metrics.updateTransactionsCount(14),
+    ])
+    l.info(
+      `Transactions and Wallets stats updated in ${Math.round((Date.now() - dateStart) / 1000)}s`
+    )
+  } catch (e) {
+    l.error('Cannot update txs and wallets stats:', e.message)
+  } finally {
+    setTimeout(loop, interval)
+  }
 }
--- src/store/postgres/Metrics.ts
+++ src/store/postgres/Metrics.ts
@@ -2,6 +2,11 @@ import {IStorageMetrics} from 'src/store/interface'
 import {Query} from 'src/store/postgres/types'
 import {fromSnakeToCamelResponse} from 'src/store/postgres/queryMapper'
 
+enum StatsTable {
+  wallets = 'wallets_count',
+  transactions = 'transactions_count',
+}
+
 export class PostgresStorageMetrics implements IStorageMetrics {
   query: Query
 
@@ -9,18 +14,34 @@ export class PostgresStorageMetrics implements IStorageMetrics {
     this.query = query
   }
 
-  getTransactionCount = async (numberOfDays = 14): Promise<any[]> => {
-    const res = await this.query(
-      `select date_trunc('day', "timestamp") as timestamp, count(1) from "transactions"
+  getTransactionCount = async (numberOfDays: number): Promise<any[]> => {
+    const rows = await this.query(
+      `select date_string as timestamp, "count" from transactions_count order by id desc limit $1;`,
+      [numberOfDays]
+    )
+    return rows
+  }
+
+  // last 14 days of funded addresses
+  getWalletsCount = async (numberOfDays: number): Promise<any[]> => {
+    const rows = await this.query(
+      `select date_string, "count" from wallets_count order by id desc limit $1;`,
+      [numberOfDays]
+    )
+    return rows.map((o: any) => ({date: o.date_string, count: o.count}))
+  }
+
+  updateTransactionsCount = async (numberOfDays: number) => {
+    const rows = await this.query(
+      `select date_trunc('day', "timestamp") as date, count(1) from "transactions"
             where "transactions"."timestamp" < date_trunc('day', CURRENT_DATE)
             group by 1 order by 1 desc limit $1`,
       [numberOfDays]
     )
-
-    return res.map(fromSnakeToCamelResponse).reverse()
+    await this.insertStats(StatsTable.transactions, rows)
   }
 
-  updateWalletsStats = async (numberOfDays = 14) => {
+  updateWalletsCount = async (numberOfDays: number) => {
     const rows = await this.query(
       `WITH base AS (
             SELECT distinct DATE_TRUNC('DAY', "timestamp") AS date, "from" AS wallet_address FROM transactions
@@ -33,38 +54,30 @@ export class PostgresStorageMetrics implements IStorageMetrics {
             WHERE date < date_trunc('day', CURRENT_DATE)
             GROUP BY date
             )
-            SELECT date, active_wallets FROM daily
+            SELECT date, active_wallets as count FROM daily
             ORDER BY date DESC
             limit $1`,
       [numberOfDays]
     )
+
+    await this.insertStats(StatsTable.wallets, rows)
+  }
+
+  private insertStats = (tableName: StatsTable, rows: Array<{date: string; count: string}>) => {
     const preparedRows = rows
-      .reverse()
-      .map((row: any) => [row.active_wallets, row.date])
+      // .reverse()
+      .map((row: any) => [row.count, row.date])
       .flat()
 
-    const multipleValues = Array(numberOfDays)
+    const multipleValues = Array(rows.length)
       .fill(null)
       .map((_, i) => `($${i * 2 + 1}, $${i * 2 + 2})`) // ($1, $2), ($3, $4), ...
       .join(',')
 
-    await this.query(
-      `insert into wallets_count (count, date_string)
+    return this.query(
+      `insert into ${tableName} (count, date_string)
             values ${multipleValues} on conflict (date_string) do nothing;`,
       [...preparedRows]
     )
-  }
-
-  // last 14 days of funded addresses
-  getWalletsCount = async (numberOfDays = 14): Promise<any[]> => {
-    const getRecords = async () => {
-      const res = await this.query(
-        `select date_string, "count" from wallets_count order by id desc limit $1;`,
-        [numberOfDays]
-      )
-      return res.map((o: any) => ({date: o.date_string, count: o.count})).reverse()
-    }
-
-    return await getRecords()
   }
 }
