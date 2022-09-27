cc145413f7fee12e6f71c373366e5f997dbb0f27
artemkolodko <temakolodko@gmail.com>
2022-09-27 12:10:59 +0100

    Increase related txs sub-query limit to 100K

--- src/indexer/indexer/metrics/stats.ts
+++ src/indexer/indexer/metrics/stats.ts
@@ -7,7 +7,6 @@ const interval = 1000 * 60 * 60 * 4
 export const statsIndexer = () => {
   l.info('Metrics indexer starting...')
   runLoop()
-  // runMetricsIndexer()
 }
 
 const runLoop = async () => {
@@ -18,41 +17,12 @@ const runLoop = async () => {
       stores[0].metrics.updateWalletsCount(limit),
       stores[0].metrics.updateTransactionsCount(limit),
       stores[0].metrics.updateAverageFee(limit),
-      stores[0].metrics.updateTopContracts(),
+      // stores[0].metrics.updateTopContracts(),
     ])
     l.info(`Daily metrics updated in ${Math.round((Date.now() - dateStart) / 1000)}s`)
   } catch (e) {
     l.error('Error on metrics update:', e.message)
   } finally {
     setTimeout(runLoop, interval)
-  }
-}
-
-const runMetricsIndexer = async () => {
-  try {
-    const batchDays = 14
-    for (let i = 0; i < 100; i++) {
-      const from = i * batchDays + batchDays
-      const to = i * batchDays
-      console.log('Index metrics: i', i, 'from', from, 'to', to)
-      const rowsWallets = await stores[0].metrics.updateWalletsCount(from, to)
-      const rowsTxs = await stores[0].metrics.updateTransactionsCount(from, to)
-      const rowsFee = await stores[0].metrics.updateAverageFee(from, to)
-      console.log(
-        'Index metrics result:',
-        'rowsWallets',
-        rowsWallets.length,
-        'rowsTxs',
-        rowsTxs.length,
-        'rowsFee',
-        rowsFee.length
-      )
-      if (rowsWallets.length + rowsTxs.length + rowsFee.length === 0) {
-        console.log('Stop metrics')
-        break
-      }
-    }
-  } catch (e) {
-    console.log('Error on metrics batch update', e)
   }
 }
